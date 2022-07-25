43e99fb553ac5ac6a82aec67b3f7671aeb197a3e
Noshhy <73740320+Noshhy@users.noreply.github.com>
2022-07-25 17:21:59 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,6 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
+    
+        apiKey: "AIzaSyBfMRMI27R7v-dY9WqMF3jJChz-K69VM0Y",
+        authDomain: "project-67-bed91.firebaseapp.com",
+        projectId: "project-67-bed91",
+        storageBucket: "project-67-bed91.appspot.com",
+        messagingSenderId: "1072012226042",
+        appId: "1:1072012226042:web:247e079423a621253d0843"
+      
 
 };
 // Initialize Firebase
